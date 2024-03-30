12573e9b26337a052007a8c4e42758ef29a771ec
CGM <mrmouse.guru@gmail.com>
2024-03-30 08:10:38 +0700

    initialize advanced listview & adapter

---
+++ app/src/main/java/com/cgm/mobile_k15pm06_ad_listview_adapter/Country.java
@@ -0,0 +1,4 @@
+package com.cgm.mobile_k15pm06_ad_listview_adapter;
+
+public class Country {
+}
---
+++ app/src/main/java/com/cgm/mobile_k15pm06_ad_listview_adapter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cgm.mobile_k15pm06_ad_listview_adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
