0807d69b7ab9b3cbd2238706dbec64a13eb2cce1
CGM <mrmouse.guru@gmail.com>
2024-03-30 08:29:12 +0700

    simple listview & built-in array adapter

--- app/src/main/java/com/cgm/mobile_k15pm06_ad_listview_adapter/Country.java
+++ app/src/main/java/com/cgm/mobile_k15pm06_ad_listview_adapter/Country.java
@@ -1,4 +1,45 @@
 package com.cgm.mobile_k15pm06_ad_listview_adapter;
 
 public class Country {
+
+    private String countryName;
+
+    // Image name (Without extension)
+    private String flagName;
+    private int population;
+
+    public Country(String countryName, String flagName, int population) {
+        this.countryName= countryName;
+        this.flagName= flagName;
+        this.population= population;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getFlagName() {
+        return flagName;
+    }
+
+    public void setFlagName(String flagName) {
+        this.flagName = flagName;
+    }
+
+    @Override
+    public String toString()  {
+        return this.countryName+" (Population: "+ this.population+")";
+    }
 }
--- app/src/main/java/com/cgm/mobile_k15pm06_ad_listview_adapter/MainActivity.java
+++ app/src/main/java/com/cgm/mobile_k15pm06_ad_listview_adapter/MainActivity.java
@@ -3,6 +3,11 @@ package com.cgm.mobile_k15pm06_ad_listview_adapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView countryListView = findViewById(R.id.countryListView);
+
+        List<Country> list = new ArrayList<Country>();
+        Country vietnam = new Country("Vietnam", "vn", 98000000);
+        Country usa = new Country("United States", "us", 320000000);
+        Country russia = new Country("Russia", "ru", 142000000);
+        list.add(vietnam);
+        list.add(usa);
+        list.add(russia);
+
+        ArrayAdapter<Country> adapter = new ArrayAdapter<Country>(this,
+                android.R.layout.simple_list_item_1, list);
+
+        countryListView.setAdapter(adapter);
     }
 }
